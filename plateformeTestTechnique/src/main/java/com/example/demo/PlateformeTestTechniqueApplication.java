6a46bf35a0add9101e7c1eeda934269eaeedfbe7
rouaarz <rzaiguiroua@gmail.com>
2025-02-08 14:36:50 +0100

    Ajout du dossier plateformeTestTechnique dans backend

---
+++ plateformeTestTechnique/src/main/java/com/example/demo/PlateformeTestTechniqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeTestTechniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformeTestTechniqueApplication.class, args);
+	}
+
+}
